e66328a22d05045276079fb06fa979f32b7e2454
munzmania <munzir.suliman@outlook.com>
2016-04-27 17:37:30 +0400

    test
    test

--- server/api/v1/controllers/error.js
+++ server/api/v1/controllers/error.js
@@ -25,6 +25,7 @@ var getUniqueErrorMessage = function(err) {
  * Get the error message from error object
  */
 exports.getErrorMessage = function(err) {
+	console.log(err);
 	var message = '';
 	
 	if (err.code) {
